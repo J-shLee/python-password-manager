29e6de370e1ca37636fb1c8ccb34c4ddc44414bb
Josh Lee <joshlee@Joshs-MacBook-Pro.local>
2023-10-26 15:51:55 +0100

    refactored retrieve_secret to put into new secrects directory

--- src/password_manager.py
+++ src/password_manager.py
@@ -2,43 +2,66 @@ import boto3
 from botocore.exceptions import ClientError
 import json
 import logging
+import os
 
-logger = logging.getLogger('MyLogger')
+logging.basicConfig()
+logger = logging.getLogger("Password_manager")
 logger.setLevel(logging.INFO)
 
 
+def handler(user_input=None):
+    valid_inputs = {
+        "e": insert_secret,
+        "r": retrieve_secret,
+        "d": delete_secret,
+        "l": list_secrets,
+    }
+
+    if user_input == None:
+        user_input = input(
+            "Please specify [e]ntry, [r]etrieval, [d]eletion, [l]isting or e[x]it: "
+        ).lower()
+
+    if user_input == "x":
+        logger.info("Thank you. Goodbye")
+        return
+    elif user_input in valid_inputs:
+        function = valid_inputs[user_input]
+        return function()
+    else:
+        logger.info("Invalid input")
+        return handler()
+
+
 def get_client():
-    secrets_manager = boto3.client('secretsmanager')
+    secrets_manager = boto3.client("secretsmanager")
     return secrets_manager
+
 
 def insert_secret(secret_identifier=None, user_id=None, password=None):
     try:
         client = get_client()
 
         if secret_identifier == None:
-            secret_identifier = input('What name would you like to give this secret?')
+            secret_identifier = input("What name would you like to give this secret? ")
 
         if user_id == None:
-            user_id = input('What is the username you\'d like to save?')
-        
+            user_id = input("What is the username you'd like to save? ")
+
         if password == None:
-            password = input('What is the password you\'d like to save?')
-        
-        json_upload = {
-            'user_id': user_id, 
-            'password': password
-        }
+            password = input("What is the password you'd like to save? ")
+
+        json_upload = {"user_id": user_id, "password": password}
 
         response = client.create_secret(
-            Name=secret_identifier,
-            SecretString=json.dumps(json_upload)
+            Name=secret_identifier, SecretString=json.dumps(json_upload)
         )
 
-
-        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-            logger.info('Secret Saved')
+        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+            logger.info("Secret Saved")
+            return handler()
     except ClientError as ce:
-        logger.error(ce)
+        logger.error(ce.response["Error"]["Message"])
     except Exception as e:
         logger.error(e)
     # finally:
@@ -50,15 +73,16 @@ def list_secrets():
         client = get_client()
         response = client.list_secrets()
 
-        if len(response['SecretList']) == 0:
+        if len(response["SecretList"]) == 0:
             logger.info("There are no secrets")
-            # return handler()
+            return handler()
         else:
-            secrets = [secret['Name'] for secret in response['SecretList']]
-            logger.info(f'{len(secrets)} secret(s) available: \n{secrets}')
-            # return handler()
+            secrets = [secret["Name"] for secret in response["SecretList"]]
+            logger.info(f"{len(secrets)} secret(s) available: \n{secrets}")
+            return handler()
     except ClientError as ce:
-        logger.error(ce)
+        logger.error(ce.response["Error"]["Message"])
+        return handler()
     # finally:
     #     return handler()
 
@@ -68,16 +92,49 @@ def delete_secret(secret_identifier=None):
         client = get_client()
 
         if secret_identifier == None:
-            secret_identifier = input('What\'s the name of the secret you would like to delete?')
+            secret_identifier = input(
+                "What's the name of the secret you would like to delete? "
+            )
 
-        response = client.delete_secret(
-            SecretId=secret_identifier
-        )
+        response = client.delete_secret(SecretId=secret_identifier)
 
-        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
             logger.info(f"Secret Deleted: {response['Name']}")
             # return handler()
     except ClientError as ce:
-        logger.error(ce)
+        logger.error(ce.response["Error"]["Message"])
     # finally:
     #     return handler()
+
+
+def retrieve_secret(secret_identifier=None):
+    try:
+        client = get_client()
+
+        if secret_identifier == None:
+            secret_identifier = input(
+                "What is the name of the secret you would like to retrieve? "
+            )
+
+        response = client.get_secret_value(SecretId=secret_identifier)
+
+        secret = json.loads(response["SecretString"])
+
+        directory = os.getcwd()
+
+        if not os.path.isdir(f"{directory}/secrets"):
+            os.mkdir(f"{directory}/secrets")
+
+        secret_txt = open(f"./secrets/{secret_identifier}.txt", "w")
+        secret_txt.write(
+            f'User ID = {secret["user_id"]} \nPassword = {secret["password"]}'
+        )
+        secret_txt.close()
+
+        logger.info(f"Secrets stored in local file {secret_identifier}.txt")
+        return handler()
+    except ClientError as ce:
+        logger.error(ce.response["Error"]["Message"])
+
+
+handler()
--- test/test_password_manager.py
+++ test/test_password_manager.py
@@ -1,15 +1,24 @@
-from src.password_manager import insert_secret, get_client, list_secrets, delete_secret
-import moto 
+from src.password_manager import (
+    insert_secret,
+    get_client,
+    list_secrets,
+    delete_secret,
+    retrieve_secret,
+    handler,
+)
+import moto
 from moto import mock_secretsmanager
 import os
 import pytest
 import boto3
 import json
 import logging
-from unittest import mock, patch
+from unittest import mock
+from unittest.mock import patch
 
-logger = logging.getLogger('MyLogger')
+logger = logging.getLogger("MyLogger")
 logger.setLevel(logging.INFO)
+
 
 @pytest.fixture(scope="function")
 def aws_credentials():
@@ -18,46 +27,49 @@ def aws_credentials():
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
 
-@mock_secretsmanager
-class TestInsertSecretsTest():
-    def test_secretsmanager_logs_correct_message_when_invoked_with_valid_inputs(self, caplog):
-        with caplog.at_level(logging.INFO):
-        
-            insert_secret('secretid', 'userid', 'password')
-            
-            assert ('Secret Saved' in caplog.text)
 
-    def test_raises_resource_exists_exception_when_name_of_secret_is_already_used(self, caplog):
+@mock_secretsmanager
+class TestInsertSecretsTest:
+    def test_secretsmanager_logs_correct_message_when_invoked_with_valid_inputs(
+        self, caplog
+    ):
+        with caplog.at_level(logging.INFO):
+            insert_secret("secretid", "userid", "password")
+
+            assert "Secret Saved" in caplog.text
+
+    def test_raises_resource_exists_exception_when_name_of_secret_is_already_used(
+        self, caplog
+    ):
         """captures as client error"""
         with caplog.at_level(logging.INFO):
+            insert_secret("secretid", "userid", "password")
+            insert_secret("secretid", "userid", "password")
 
-            insert_secret('secretid', 'userid', 'password')
-            insert_secret('secretid', 'userid', 'password')
-        
-            assert ('A resource with the ID you requested already exists.' in caplog.text)
+            assert "A resource with the ID you requested already exists." in caplog.text
 
     def test_raises_parameter_validation_error_if_invalid_inputs_provided(self, caplog):
         """captures as client error"""
         with caplog.at_level(logging.INFO):
+            insert_secret(["secretid"], "user_id", "password")
 
-            insert_secret(['secretid'], 'user_id', 'password')
+            assert "Parameter validation failed" in caplog.text
 
-            assert ('Parameter validation failed' in caplog.text)
 
 @mock_secretsmanager
-class TestListSecrets():
+class TestListSecrets:
     def test_returns_correct_message_when_no_secrets_stored(self, caplog):
         with caplog.at_level(logging.INFO):
             list_secrets()
 
-            assert ('There are no secrets' in caplog.text)
+            assert "There are no secrets" in caplog.text
 
-    def test_returns_correct_message_when_no_secrets_stored(self, caplog):
+    def test_returns_correct_message_when_secrets_stored(self, caplog):
         with caplog.at_level(logging.INFO):
-            insert_secret('a', 'a', 'a')
+            insert_secret("a", "a", "a")
             list_secrets()
 
-            assert ("1 secret(s) available: \n['a']" in caplog.text)
+            assert "1 secret(s) available: \n['a']" in caplog.text
 
     # def test_raises_client_error(self, caplog):
     #     with patch('../src.password_manager.get_client', return_value=None):
@@ -66,19 +78,80 @@ class TestListSecrets():
 
     #             list_secrets()
 
+
 @mock_secretsmanager
-class TestDeleteSecrets():
+class TestDeleteSecrets:
     def test_returns_correct_message_when_secret_successfully_deleted(self, caplog):
         with caplog.at_level(logging.INFO):
-            insert_secret('a', 'a', 'a')
-            delete_secret('a')
+            insert_secret("a", "a", "a")
+            delete_secret("a")
 
-            assert ('Secret Deleted: a' in caplog.text)
+            assert "Secret Deleted: a" in caplog.text
 
     def test_captures_client_error_when_secret_does_not_exist(self, caplog):
         with caplog.at_level(logging.INFO):
-            delete_secret('a')
+            delete_secret("a")
 
-            assert ("ResourceNotFound" in caplog.text)
+            assert "Secrets Manager can't find the specified secret" in caplog.text
 
     # def test_raises_client_error(self, caplog):
+
+
+@mock_secretsmanager
+class TestRetrieveSecret:
+    def test_returns_correct_message_when_secret_successfully_retreived(self, caplog):
+        with caplog.at_level(logging.INFO):
+            insert_secret("a", "a", "a")
+            retrieve_secret("a")
+
+            assert f"Secrets stored in local file a.txt" in caplog.text
+
+    def test_creates_new_file_containing_correct_secret_info(self):
+        insert_secret("a", "a", "a")
+        retrieve_secret("a")
+        directory = os.getcwd()
+        assert os.path.exists(f"{directory}/secrets/a.txt") == True
+
+    def test_correct_content_is_inside_the_file(self):
+        insert_secret("a", "a", "a")
+        retrieve_secret("a")
+        directory = os.getcwd()
+
+        secret_txt = open(f"{directory}/a.txt", "r")
+        lines = secret_txt.readlines()
+        assert lines[0] == "User ID = a \n"
+        assert lines[1] == "Password = a"
+
+    def test_returns_client_error_if_secret_doesnt_exist(self, caplog):
+        with caplog.at_level(logging.INFO):
+            retrieve_secret("a")
+
+            assert "Secrets Manager can't find the specified secret" in caplog.text
+
+
+class TestHandler:
+    def test_handler_invokes_insert_secret_with_user_input_e(self):
+        with patch("src.password_manager.insert_secret", return_value=True) as mock:
+            assert handler("e") == True
+            assert mock.call_count == 1
+
+    def test_handler_invokes_list_secrets_with_user_input_l(self):
+        with patch("src.password_manager.list_secrets", return_value=True) as mock:
+            assert handler("l") == True
+            assert mock.call_count == 1
+
+    def test_handler_invokes_delete_secrets_with_user_input_d(self):
+        with patch("src.password_manager.delete_secret", return_value=True) as mock:
+            assert handler("d") == True
+            assert mock.call_count == 1
+
+    def test_handler_invokes_delete_secrets_with_user_input_r(self):
+        with patch("src.password_manager.retrieve_secret", return_value=True) as mock:
+            assert handler("r") == True
+            assert mock.call_count == 1
+
+    def test_handler_logs_correct_message_on_exit(self, caplog):
+        with caplog.at_level(logging.INFO):
+            handler("x")
+
+            assert "Thank you. Goodbye" in caplog.text
